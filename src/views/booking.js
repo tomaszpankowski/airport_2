e91ace138c043004cec7d95cdb16ebdf5c1393e0
tomaszpankowski <pankowski.email@gmail.com>
2022-02-10 15:21:37 +0100

    layout done

--- src/views/booking.js
+++ src/views/booking.js
@@ -1,11 +1,12 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import BookingS1 from "../components/booking-s1";
 
 class Booking extends Component{
     render(){
         return(        
-            <Container fluid className="booking-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <BookingS1/>
             </Container>    
         );
     }
--- src/views/destinations.js
+++ src/views/destinations.js
@@ -1,11 +1,19 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import DestinationsS1 from "../components/destinations-s1";
+import DestinationsS2 from "../components/destinations-s2";
+import DestinationsS3 from "../components/destinations-s3";
+import DestinationsS4 from "../components/destinations-s4";
+
 
 class Destinations extends Component{
     render(){
         return(        
-            <Container fluid className="destinations-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <DestinationsS1/>
+                <DestinationsS2/>
+                <DestinationsS3/>
+                <DestinationsS4/>
             </Container>    
         );
     }
